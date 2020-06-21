35590d96d41652f615bd4f5885cc569318506cac
Harry Ronchetti <harry@ne6.digital>
2020-06-21 18:04:10 +0100

    Balustrading added

--- src/pages/products/balustrading.js
+++ src/pages/products/balustrading.js
@@ -5,45 +5,76 @@ export default function Balustrading() {
   const productOptions = [
     {
       optionId: 1,
-      optionName: 'B500G Baluster',
+      optionName: 'B740 Baluster',
       optionTechnicalDrawings: [
         {
           drawingId: 1,
-          technicalDrawingName: 'Default',
-          technicalDrawingSVG: 'porticos/portico-type-a.svg',
+          technicalDrawingName: 'Baluster',
+          technicalDrawingSVG: 'balustrading/B740-baluster.svg',
+        },
+        {
+          drawingId: 2,
+          technicalDrawingName: 'Pier',
+          technicalDrawingSVG: 'balustrading/B740-pier.svg',
+        },
+        {
+          drawingId: 3,
+          technicalDrawingName: 'Pier Caps',
+          technicalDrawingSVG: 'balustrading/B740-pier-caps.svg',
         },
       ],
     },
     {
       optionId: 2,
-      optionName: 'B610G Baluster',
+      optionName: 'SB1 Baluster',
       optionTechnicalDrawings: [
         {
           drawingId: 1,
           technicalDrawingName: 'Default',
-          technicalDrawingSVG: 'porticos/portico-type-a.svg',
+          technicalDrawingSVG: 'balustrading/SB1-baluster.svg',
         },
       ],
     },
     {
       optionId: 3,
-      optionName: 'B740G Baluster',
+      optionName: 'SB2 Baluster',
       optionTechnicalDrawings: [
         {
           drawingId: 1,
-          technicalDrawingName: 'Default',
-          technicalDrawingSVG: 'porticos/portico-type-a.svg',
+          technicalDrawingName: 'Baluster',
+          technicalDrawingSVG: 'balustrading/SB2-baluster.svg',
+        },
+        {
+          drawingId: 2,
+          technicalDrawingName: 'Pier',
+          technicalDrawingSVG: 'balustrading/SB2-pier.svg',
         },
       ],
     },
     {
       optionId: 4,
-      optionName: 'Balustrade pier cap options',
+      optionName: 'SB3 Baluster',
       optionTechnicalDrawings: [
         {
           drawingId: 1,
-          technicalDrawingName: 'A1',
-          technicalDrawingSVG: 'porticos/portico-type-a.svg',
+          technicalDrawingName: 'Baluster',
+          technicalDrawingSVG: 'balustrading/SB3-baluster.svg',
+        },
+        {
+          drawingId: 2,
+          technicalDrawingName: 'Pier',
+          technicalDrawingSVG: 'balustrading/SB3-pier.svg',
+        },
+      ],
+    },
+    {
+      optionId: 5,
+      optionName: 'SB1, SB2, SB3 Pier Bases',
+      optionTechnicalDrawings: [
+        {
+          drawingId: 1,
+          technicalDrawingName: 'Default',
+          technicalDrawingSVG: 'balustrading/SB-pier-bases.svg',
         },
       ],
     },
@@ -54,7 +85,7 @@ export default function Balustrading() {
       title="Dry/Wet Cast Stone Balustrading Products and Bespoke Design"
       description="For bespoke design call us on 01327 842585 or use the form on our contact page"
       product="Balustrading"
-      productDecription="3 Standard products that make an entrance"
+      productDecription="4 Standard products"
       productOptions={productOptions}
       url="/products/balustrading"
     />
